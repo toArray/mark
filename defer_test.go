81e8ed7fca23d9fce22cd7835e454243ce17ea73
toArray <820506305@qq.com>
2022-01-29 15:35:15 +0800

    defer

---
+++ defer_test.go
@@ -0,0 +1,24 @@
+package luoqiangMark
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*
+TestDeferFor
+以下代码打印什么
+
+ */
+func TestDeferFor(t *testing.T)  {
+	for i:=0;i<5;i++{
+		defer fmt.Printf("%d ",i)
+	}
+
+	//这边先还有最后一次i++ 所有5
+	for i:=0;i<5;i++{
+		defer func() {
+			fmt.Printf("%d ",i)
+		}()
+	}
+}
